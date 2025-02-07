2af47a334a7bb0c8626ce68ac3b6a34fbd09bf87
conztantin <conztantinsixten@gmail.com>
2025-02-07 12:08:47 +0100

    destinations in progress

--- destinations.js
+++ destinations.js
@@ -11,16 +11,12 @@ document.querySelector("#root").append(mainElm)
 fetch(`/data/${id}.json`)
     .then(response => response.json())
     .then(data =>  {
+        
         let sectionElm = document.createElement("section")
-        sectionElm.innerHTML = data.destinations.map(destination => `
-            <div class="location">
-            <figure class="location__img>
-            <img src="/img/${destination.image}">
-            </figure>
-            <div class="location__info">
-            <h1 class="location__info--headline></h1>
-            </div>
-                </div>`).join("")
+        sectionElm.innerHTML = `
+        <img src="${data.image}" alt="">
+
+        `
             mainElm.append(sectionElm)
     })
 
