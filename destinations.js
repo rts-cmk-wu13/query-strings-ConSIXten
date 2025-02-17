114d62ef5c71e25941b31025a3d497dac57033d7
conztantin <conztantinsixten@gmail.com>
2025-02-17 11:07:42 +0100

    final details finished - postion of "favorit" on the destinations page is not positioned correctly

--- destinations.js
+++ destinations.js
@@ -1,4 +1,3 @@
-
 let search = window.location.search
 let params = new URLSearchParams(search)
 console.log(params);
@@ -15,15 +14,21 @@ fetch(`/data/${id}.json`)
         let sectionElm = document.createElement("section")
         sectionElm.classList.add("full-width");
         sectionElm.innerHTML = `
-        <figure class="half info__site--img">
-            <img src="${data.image}" alt="">
-        </figure>
+        <div>
+            <figure class="half info__site--img">
+                <img src="${data.image}" alt="">
+            </figure>
+            <div class="info__site--favorite">
+            <img src="like.png" class="info__site--icon" data-id="${id}">
+            <p class="info__site--text">FAVORIT</p>
+            </div>
+        </div>
         <div class="half section">
             <h1 class="section text-center">${data.destination}</h1>
             <h2 class="margin-top text-center">${data.title}</h2>
             <h3 class="margin-top text-center">${data.subtitle}</h3>
             <p class="margin-top">${data.text}</p>
-            <h4 class="margin-top">Facilities</h4>
+            <h4 class="margin-top">facilities</h4>
             <ul>
                 ${data.facilities.map(item => `<li>${item}</li>`).join("")}
             </ul>
--- script.js
+++ script.js
@@ -1,23 +1,51 @@
+let mainElm = document.createElement("main");
+document.querySelector("#root").append(mainElm);
 
-let mainElm = document.createElement("main")
-document.querySelector("#root").append(mainElm)
+let headerElm = document.createElement("header");
+headerElm.innerHTML = `<h1>Apartments for rent</h1>`;
+mainElm.append(headerElm);
+
+// Load likede destinationer fra localStorage
+let likedDestinations = new Set(JSON.parse(localStorage.getItem("likedDestinations")) || []);
 
 fetch("/data/destinations.json")
     .then(response => response.json())
-    .then(data => {
-        let sectionElm = document.createElement("section")
-
-        sectionElm.innerHTML = data.destinations.map(destination => `
+    .then(({ destinations }) => {
+        let sectionElm = document.createElement("section");
+        sectionElm.innerHTML = destinations.map(({ id, image }) => `
             <div class="card">
-            <figure class="card__img">
-            <img src="/img/${destination.image}">
-            </figure>
-            <div class="card__nav">
-            <img src="like.png" class="card__nav--icon">
-            <a class="card__nav--link" href="destination.html?id=${destination.id}">
-                MORE
-                </a>
+                <figure class="card__img">
+                    <img src="/img/${image}" alt="">
+                </figure>
+                <div class="card__nav">
+                    <img src="like.png" class="card__nav--icon" data-id="${id}">
+                    <a class="card__nav--link" href="destination.html?id=${id}">MORE</a>
                 </div>
-                </div>`).join("")
-            mainElm.append(sectionElm)
-    })
+            </div>`).join("");
+        mainElm.append(sectionElm);
+
+        // Vælg alle ikoner og tilføjer eventlistener
+        document.querySelectorAll(".card__nav--icon").forEach(icon => {
+            let id = icon.dataset.id;
+
+            // Tilføjer klassen "liked", hvis destinationen er i favoritter
+            if (likedDestinations.has(id)) {
+                icon.classList.add("liked");
+            }
+
+            // skifter on-click
+            icon.addEventListener("click", function () {
+                if (likedDestinations.has(id)) {
+                    likedDestinations.delete(id); // fjerner fra favoritter
+                    this.classList.remove("liked");
+                } else {
+                    likedDestinations.add(id); // tilføjer til favoritter
+                    this.classList.add("liked");
+                }
+
+                // Gemmer favoritter i localStorage
+                localStorage.setItem("likedDestinations", JSON.stringify([...likedDestinations]));
+                console.log(likedDestinations)
+            });
+        });
+    });
